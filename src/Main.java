88394ed25ddfd4a48f15b7d4731d3fb6f68f5e7c
Dmitrii Dmitriev <seriouskeyn@mail.ru>
2017-01-29 20:14:20 +0300

    v2.0

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,38 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double x;
-        double s = (int)(Math.random()*9);
-        boolean ff = false;
-        System.out.println(s);
+        int number;
+        boolean correctLevel;
+        int level;
+        System.out.println("Игра угадай число от 0 до 10");
+        System.out.println("Выбирете уровень сложности \n 1. от 0 до 10 \n 2. от 0 до 20 \n 3. от 0 до 30");
         do {
-            x = sc.nextDouble();
-            if (x>s)System.out.println("Бери меньше");
-            else if (x == s) {
-                ff = true;
+            level = sc.nextInt();
+            correctLevel  = correctLevel(level);
+            if (correctLevel == true) break;
+            else System.out.println("Данного уровня сложности не существует");
+        } while (true);
+        int n = 0;
+        if (level == 1) {n = 10;System.out.println("Вы выбрали уровень сложности 1 (от 0 до 10)");}
+        if (level == 2) {n = 20;System.out.println("Вы выбрали уровень сложности 2 (от 0 до 20)");}
+        if (level == 3) {n = 30;System.out.println("Вы выбрали уровень сложности 3 (от 0 до 30)");}
+        System.out.println("Введите число");
+        int randomNumber = (int)(Math.random()*n);
+        boolean correct = false;
+        do {
+            number = sc.nextInt();
+            if (number > randomNumber)System.out.println("Бери меньше");
+            else if (number == randomNumber) {
+                correct = true;
                 System.out.println("Ты угадал");
-                break;
             }
             else System.out.println("Бери больше");
-        } while (ff != true);
+        } while (correct != true);
+    }
+    public static boolean correctLevel(int level){
+        if (level == 1) return true;
+        else if (level == 2) return true;
+        else if (level == 3) return true;
+        else return false;
     }
 }
