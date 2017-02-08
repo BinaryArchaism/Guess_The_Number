e8a579b2bd472fb11d992b8a018b3970b8b1b0d1
Dmitrii Dmitriev <seriouskeyn@mail.ru>
2017-02-08 23:28:40 +0300

    v5.5
    Поправил алгоритм. Make algorithm correct.

--- src/Computer.java
+++ src/Computer.java
@@ -2,17 +2,20 @@
  * Created by Dmitrii on 03.02.2017.
  */
 public class Computer {
+    int min, max;
     public int randomNumber(int level) {
         return  (int)(Math.random() * (10 * level));
     }
     public int guessingComputer(int status, int level, int number) {
-        int min = 0;
+        min = 0;
+        max = level*10;
         if (status == 1) {
-            min = (level*10 + number) / 2;
-            return (level*10 + number)/2;
+            min = number;
+            return (max+min)/2;
         }
         else if (status == 2) {
-            return ((level*10 + min) - number )/2;
+            max = number;
+            return (max + min)/2;
         }
         else {
             System.out.println("Компьютер угадал ваше число! Спасибо за игру!");
